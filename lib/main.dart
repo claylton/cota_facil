1ef3a884a928af6be93e5b90fa8d88e45527f8be
Claylton Santos <claylton.santos@idwall.co>
2025-04-10 17:28:32 -0300

    fix: :bug: add MyHttpOverrides

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:cota_facil/presentation/pages/home_page.dart';
 import 'package:cota_facil/presentation/pages/splash_screen_page.dart';
 import 'package:cota_facil/theme/light_theme.dart';
 import 'package:flutter/material.dart';
+import 'dart:io';
 
 void main() {
+   HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
@@ -22,5 +24,13 @@ class MyApp extends StatelessWidget {
         'home_page': (context) => const HomePage(),
       },
     );
+  }
+}
+
+ class MyHttpOverrides extends HttpOverrides{
+  @override
+  HttpClient createHttpClient(SecurityContext? context){
+    return super.createHttpClient(context)
+      ..badCertificateCallback = (X509Certificate cert, String host, int port)=> true;
   }
 }
